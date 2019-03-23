9c9d87e3b69a3c0821a008fbe580bdac076d6403
ferdy <xv.ferdy@gmail.com>
2019-03-23 13:24:30 +0700

    Only add circle

--- script.js
+++ script.js
@@ -1,108 +1,55 @@
-let shape = document.getElementById('shape');
-// var shapeRec = document.getElementById('shapeRec');
-// let button = document.querySelector('button');
+alert('Waktu kamu hanya 2 detik untuk masing-masing lingkaran dan kotak.\nJika sudah siap klik OK.');
 
-//game dimulai
-// function game() {
-//     this.style.display = 'none';
-//     random();
-//     muncul();
-// }
+var circle = document.getElementById('circle');
+var rect = document.getElementById('rect');
 
+//variabel awal
 var start = new Date().getTime();
 
 function gameover() {
-    alert('d');
+    alert('Apa yang kamu tunggu!?');
+    location.reload();
 }
-
 var gameover = setTimeout(gameover, 2000);
 
-
-
-
-
-
-
-//random position
+//ketika linkaran di klik
 function random(event) {
-    let top = Math.floor(Math.random() * 620);
+    let top = Math.floor(Math.random() * 583);
     let left = Math.floor(Math.random() * 620);
     event.style.left = left + 'px';
     event.style.top = top + 'px';
 
-    var end = new Date().getTime();
-
-    var timeTaken = (end - start) / 1000;
-    document.getElementById("timeTaken").innerHTML = timeTaken + "s";
-
-    // let shape = document.getElementById('shape');
-    // hilangOtomatis(shape);
-    if (timeTaken > 1) {
-        document.getElementById("ket").innerHTML = "lama bener anjg";
-        muncul(shape);
-    } else {
-        document.getElementById("ket").innerHTML = "fast juga kamu";
-        muncul(shape);
-    }
-
+    ketWaktu();
 }
 
-// function randomRec(event) {
-//     let top = Math.floor(Math.random() * 620);
-//     let left = Math.floor(Math.random() * 620);
-//     shapeRec.style.left = left + 'px';
-//     shapeRec.style.top = top + 'px';
-// }
+//membuat keterangan waktu
+function ketWaktu() {
+    var end = new Date().getTime();
+    var timeTaken = (end - start) / 1000;
+    document.getElementById("timeTaken").innerHTML = "Waktu: " + timeTaken + " s";
+    if (timeTaken > 1) {
+        document.getElementById("ket").innerHTML = "Keterangan: Lambat!";
+        muncul();
+    } else {
+        document.getElementById("ket").innerHTML = "Keterangan: Bagus!";
+        muncul();
+    }
+}
 
-//muncul
-function muncul(event) {
-
-    // setTimeout(function () {
-    event.style.display = 'block';
-    // }, 100);
-
+//membuat waktu terbaru
+function muncul() {
     start = new Date().getTime();
 
     function gameover2() {
-        alert('d');
+        alert('WAKTU HABIS.\nKlik OK untuk memulai ulang game');
+        location.reload();
     }
     gameover = setTimeout(gameover2, 2000);
-
-
 }
 
-// //muncul delay
-// function munculDelay() {
-//     setTimeout(muncul, 1000);
-// }
-
-//hilang secara otomatis
-function hilangOtomatis(event) {
-    var timeOutId = setTimeout(function () {
-        event.style.display = 'none';
-    }, 1000);
-}
-
-
-
-
-
-shape.addEventListener('click', function () {
-    let shape = document.getElementById('shape');
-
-
+//event
+circle.addEventListener('click', function () {
+    let circle = document.getElementById('circle');
     clearTimeout(gameover);
-    random(shape);
-
-    // hilangOtomatis(shape);
-    // muncul(shape);
+    random(circle);
 });
-
-
-// shapeRec.addEventListener('click', function () {
-//     let shapeRec = document.getElementById('shapeRec');
-//     random(shapeRec);
-//     hilangOtomatis();
-// });
-
-button.addEventListener('click', muncul);
